5940f4709c9b2847d39dddc35a7a4bd193066a75
Tuncay Ekmez <tuncayekmez@Tuncay-MacBook-Pro.local>
2024-11-30 14:44:49 +0300

    feat: Add auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
